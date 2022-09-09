e0841108ebda3f4848a8111a430221cfbcf8a902
Aye <>
2022-09-09 14:45:44 +0800

    fixed

--- easdktool/android/src/main/java/com/example/easdktool/EasdktoolPlugin.java
+++ easdktool/android/src/main/java/com/example/easdktool/EasdktoolPlugin.java
@@ -213,7 +213,7 @@ public class EasdktoolPlugin implements FlutterPlugin, MethodCallHandler {
     final String kConnectState = "ConnectState";
     final String kArgumentsError = "ArgumentsError";
     final String kBluetoothState = "BluetoothState";
-    const String kBingdingWatchResponse = "BingdingWatchResponse";
+    final String kBingdingWatchResponse = "BingdingWatchResponse";
     final String kSetWatchResponse = "SetWatchResponse";
     final String kGetWatchResponse = "GetWatchResponse";
     final String kGetBigWatchData = "GetBigWatchData";
@@ -3126,7 +3126,7 @@ public class EasdktoolPlugin implements FlutterPlugin, MethodCallHandler {
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("dataType", type);
                 jsonObject.put("respondCodeType", respondCodeType);
-                if(dataType == kEADataInfoTypeBingWatch){
+                if(type == 6){
                     channel.invokeMethod(kBingdingWatchResponse, jsonObject.toJSONString());
                 }else {
                     channel.invokeMethod(kSetWatchResponse, jsonObject.toJSONString());
